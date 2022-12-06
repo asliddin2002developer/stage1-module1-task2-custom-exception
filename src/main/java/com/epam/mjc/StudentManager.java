5c3e080099236cd0fcf7b6a3f96b220d50216bd2
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-12-06 12:30:55 +0500

    Custom Exception added

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -6,7 +6,18 @@ public class StudentManager {
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
   public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+      Student id = Student.getValueOf(studentID);
+      if (id != null){
+        return id;
+      }else {
+        throw new StudentNotFoundException(
+                "Could not find student with ID " + studentID
+        );
+      }
+
+
+
+
   }
 
   public static void main(String[] args) {
